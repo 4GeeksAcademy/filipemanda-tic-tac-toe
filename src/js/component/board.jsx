f41a19d68f428e5946740934280c10e317bb66aa
leosequani <156928045+leosequani@users.noreply.github.com>
2024-02-29 14:16:54 +0000

    hello

--- src/js/component/board.jsx
+++ src/js/component/board.jsx
@@ -9,31 +9,48 @@ const Board = () => {
 
     const [turn, setTurn] = useState("X")
     const [squareValues, setSquareValues] = useState(["", "", "", "", "", "", "", "", ""]);
-    const checkWinner = () => {
-       let possibleCombinations = [
+    const [winner, setWinner] = useState("");
+
+    const checkWinner = (gameArray) => {
+        let possibleCombinations = [
             [1, 1, 1, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 1, 1, 1, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 1, 1, 1],
             [1, 0, 0, 1, 0, 0, 1, 0, 0],
             [0, 1, 0, 0, 1, 0, 0, 1, 0],
-            [0, 0, 1, 0, 0, 1, 0, 0, 1],            
-            [1, 0, 0, 0, 1, 0, 0, 0, 1], 
+            [0, 0, 1, 0, 0, 1, 0, 0, 1],
+            [1, 0, 0, 0, 1, 0, 0, 0, 1],
             [0, 0, 1, 0, 1, 0, 1, 0, 0]
         ];
-        let counter = 0 ;
-        
-        possibleCombinations.forEach(e=> {
-            console.log(e)
-            for (const [element,index] e.entries());
+
+        possibleCombinations.forEach(winningArray => {
+            let counter = 0;
+            for (const [index, element] of winningArray.entries()) {
+                if (element == 1) {
+                    if (gameArray[index] == turn) {
+                        counter++;
+                        if (counter > 2) {
+                            handleWinner(turn)
+                        }
+                    }
+                }
+            };
         })
-        
-        
+
+    }
+
+    const resetButton = () => {
+        setTurn("X")
+        setSquareValues(["", "", "", "", "", "", "", "", ""]);
+        setWinner("");
     }
 
 
+
+
     const handleClick = (num) => {
-        
-        
+
+
         if (squareValues[num] !== "") return;
         let arr = [...squareValues]
         if (turn === "X") {
@@ -43,14 +60,26 @@ const Board = () => {
             arr[num] = "0";
             setTurn("X")
         }
-        setSquareValues(arr)
-        checkWinner();
+        setSquareValues(arr);
+
+        checkWinner(arr);
+
+
+    }
+    const handleWinner = (winner) => {
+        setWinner(winner)
     }
 
 
     return (
         <>
             <div className="container-fluid ">
+                <div className="anouncement">
+                    {
+                        winner ? <h1>Game over! The Winner is: {winner == "X" ? "X" : "0"}</h1> : null
+                    }
+
+                </div>
                 <div className="row mb-5">
                     <div className="row">
                         <div className="col d-flex justify-content-center">
@@ -69,7 +98,7 @@ const Board = () => {
 
 
                 <div className="row"  >
-                    <div className="col d-flex justify-content-center border-bottom border-end border-5" onClick={(e) => handleClick("0")} ><i className={`${squareValues[0] === 'X' ? 'fas fa-times' : squareValues[0] === '0' ? 'far fa-circle' : ''}`}></i>.</div>
+                    <div className="col w-25 h-25 d-flex justify-content-center border-bottom border-end border-5" onClick={(e) => handleClick("0")} ><i className={`${squareValues[0] === 'X' ? 'fas fa-times' : squareValues[0] === '0' ? 'far fa-circle' : ''}`}></i>.</div>
                     <div className="col d-flex justify-content-center border-bottom border-end border-5" onClick={(e) => handleClick("1")}><i className={`${squareValues[1] === 'X' ? 'fas fa-times' : squareValues[1] === '0' ? 'far fa-circle' : ''}`}></i>.</div>
                     <div className="col d-flex justify-content-center border-bottom border-5" onClick={(e) => handleClick("2")}><i className={`${squareValues[2] === 'X' ? 'fas fa-times' : squareValues[2] === '0' ? 'far fa-circle' : ''}`}></i>.</div>
 
@@ -81,9 +110,14 @@ const Board = () => {
 
                 </div>
                 <div className="row"  >
-                    <div className="col d-flex justify-content-center border-bottom border-end border-5" onClick={(e) => handleClick("6")} ><i className={`${squareValues[6] === 'X' ? 'fas fa-times' : squareValues[6] === '0' ? 'far fa-circle' : ''}`}></i>.</div>
-                    <div className="col d-flex justify-content-center border-bottom border-end border-5" onClick={(e) => handleClick("7")}><i className={`${squareValues[7] === 'X' ? 'fas fa-times' : squareValues[7] === '0' ? 'far fa-circle' : ''}`}></i>.</div>
-                    <div className="col d-flex justify-content-center border-bottom border-5" onClick={(e) => handleClick("8")}><i className={`${squareValues[8] === 'X' ? 'fas fa-times' : squareValues[8] === '0' ? 'far fa-circle' : ''}`}></i>.</div>
+                    <div className="col d-flex justify-content-center border-end border-5" onClick={(e) => handleClick("6")} ><i className={`${squareValues[6] === 'X' ? 'fas fa-times' : squareValues[6] === '0' ? 'far fa-circle' : ''}`}></i>.</div>
+                    <div className="col d-flex justify-content-center border-end border-5" onClick={(e) => handleClick("7")}><i className={`${squareValues[7] === 'X' ? 'fas fa-times' : squareValues[7] === '0' ? 'far fa-circle' : ''}`}></i>.</div>
+                    <div className="col d-flex justify-content-center border-5" onClick={(e) => handleClick("8")}><i className={`${squareValues[8] === 'X' ? 'fas fa-times' : squareValues[8] === '0' ? 'far fa-circle' : ''}`}></i>.</div>
+                </div>
+                <div className="row">
+                    <div className="col d-flex justify-content-center mt-2">
+                        <button onClick={()=>{resetButton()}}>Reset</button>
+                    </div>
                 </div>
             </div>
 
